37904b45fba893a708079889dad439e1a80f9471
maying <my@mayingdeMacBook-Pro.local>
2020-08-22 21:00:49 -0400

    function with button

--- app/src/main/java/com/myappcompany/rob/exampleproject/MainActivity.java
+++ app/src/main/java/com/myappcompany/rob/exampleproject/MainActivity.java
@@ -2,8 +2,26 @@ package com.myappcompany.rob.exampleproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convertCurreny(View view){
+        Log.i("Info","Button Pressed");
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        String amountInrmb = editText.getText().toString();
+
+        double amamountInrmbdouble = Double.parseDouble(amountInrmb);
+        double amamountIndoldouble = amamountInrmbdouble / 7;
+
+        String amountIndolstring = String.format("%.2f",amamountIndoldouble);
+
+        Toast.makeText(this,"$" +amountIndolstring+" is rmb" +amountInrmb, Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
