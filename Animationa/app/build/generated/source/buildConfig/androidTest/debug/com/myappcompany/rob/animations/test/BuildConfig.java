fc673ddb939126282b4eae93081a5dbc60085e9c
YING MA <37651084+maying0120@users.noreply.github.com>
2020-08-31 09:41:14 +0800

    Add files via upload

---
+++ Animationa/app/build/generated/source/buildConfig/androidTest/debug/com/myappcompany/rob/animations/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.myappcompany.rob.animations.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.myappcompany.rob.animations.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
