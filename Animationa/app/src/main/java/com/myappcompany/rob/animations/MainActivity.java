c6e2f92520d816fca251426e41c738343f8804d8
YING MA <37651084+maying0120@users.noreply.github.com>
2020-08-31 09:37:14 +0800

    Add files via upload

---
+++ Animationa/app/src/main/java/com/myappcompany/rob/animations/MainActivity.java
@@ -0,0 +1,46 @@
+package com.myappcompany.rob.animations;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean bartIsShowing = true;
+
+    public void fade(View view) {
+
+        Log.i("Info", "Imageview tapped");
+
+        ImageView bartImageView = (ImageView) findViewById(R.id.bartImageView);
+
+        ImageView homerImageView = (ImageView) findViewById(R.id.homerImageView);
+
+        if (bartIsShowing) {
+
+            bartIsShowing = false;
+
+            bartImageView.animate().alpha(0).setDuration(2000);
+
+            homerImageView.animate().alpha(1).setDuration(2000);
+
+        } else {
+
+            bartIsShowing = true;
+
+            bartImageView.animate().alpha(1).setDuration(2000);
+
+            homerImageView.animate().alpha(0).setDuration(2000);
+
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
